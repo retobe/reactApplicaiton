c1e5994870f92cb9800f8ef5d1a40a550550917e
retobe <retobeyt@gmail.com>
2023-10-19 09:20:37 -0400

    Added onSelectItem prop

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["Yemen", "Saudi Arabia", "Palestine", "Iraq"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
 
   return (
     <div>
-      <ListGroup items={items} heading="Arab Countries" />
+      <ListGroup
+        items={items}
+        heading="Arab Countries"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
