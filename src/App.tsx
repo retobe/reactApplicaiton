8dd3166aefa0471600d1422acb4c984a857aaded
retobe <retobeyt@gmail.com>
2023-10-19 15:51:37 -0400

    Finished App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
+import Contact from "./components/Contact";
+
 function App() {
   return (
     <div>
       <Navbar></Navbar>
       <Home></Home>
       <About></About>
+      <Contact></Contact>
     </div>
   );
 }
---
+++ src/components/Contact.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h1 className="title">Contact ya boy!</h1>
+      <p className="description">
+        Go ahead and click on them socials and give yo boy a like and usb fr
+      </p>
+      <div className="contact-items">
+        <a
+          href="https://www.youtube.com/watch?v=Zw0ixrzpMRw&ab_channel=MoreSidemen"
+          id="facebook"
+          className="fa fa-facebook"
+        ></a>
+        <a
+          href="https://www.youtube.com/watch?v=Zw0ixrzpMRw&ab_channel=MoreSidemen"
+          id="twitter"
+          className="fa fa-twitter"
+        ></a>
+        <a
+          href="https://www.youtube.com/watch?v=Zw0ixrzpMRw&ab_channel=MoreSidemen"
+          id="pinterest"
+          className="fa fa-pinterest"
+        ></a>
+        <a
+          href="https://www.youtube.com/watch?v=Zw0ixrzpMRw&ab_channel=MoreSidemen"
+          id="steam"
+          className="fa fa-steam"
+        ></a>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
