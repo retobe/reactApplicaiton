f5e880920845113b5d5adcf5747d1cc9f9a4d7fd
retobe <retobeyt@gmail.com>
2023-10-19 09:17:07 -0400

    Update ListGroups & App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["Yemen", "Saudi Arabia", "Palestine", "Iraq"];
+
   return (
     <div>
-      <ListGroup></ListGroup>
+      <ListGroup items={items} heading="Arab Countries" />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,37 @@
-function ListGroup() {
+import { useState } from "react";
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup({ items, heading }: Props) {
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  //Event Handler
+  const handleClick = (event: MouseEvent) => console.log(event);
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 ? <p>No items found</p> : null}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
